8dc3034c3ecd97b24b58fd09e77a5588a9893d91
mousammm <mousamssdia@gmail.com>
2026-05-02 02:59:38 +0530

    split file

--- main.c
+++ main.c
@@ -2,42 +2,30 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "cpu.h"
+#include "fillMem.h"
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-
-typedef struct {
-  u8 a; /* accumulator */
-  u8 x; /* index */
-  u8 y; /* index */
-  u16 pc; /* program counter */
-} CPU;
-
-CPU cpu = { 0x00, 0x00, 0x00, 0x1000 };
-static u8 RAM[0x10000] = {0};
-
-void init() {
-  RAM[0x1000] = 0x49; /* LDA 0x44 */
-  RAM[0x1001] = 0x44;
-
-  RAM[0x1002] = 0x42; /* LDX 0x46 */
-  RAM[0x1003] = 0x45;
-
-  RAM[0x1004] = 0x40; /* LDY 0x45 */
-  RAM[0x1005] = 0x46;
-
-  RAM[0x1006] = 0x8D; /* STA 4400 put backward*/
-  RAM[0x1007] = 0x00;
-  RAM[0x1008] = 0x44;
-
-  RAM[0x1009] = 0x8E; /* STX 4401 put backward*/
-  RAM[0x100A] = 0x01;
-  RAM[0x100B] = 0x44;
-
-  RAM[0x100C] = 0x8C; /* STY 4402 put backward*/
-  RAM[0x100D] = 0x02;
-  RAM[0x100E] = 0x44;
-}
+// typedef enum {
+//   IMMEDIATE = 2,
+//   ABSOLUTE = 3,
+// } mode;
+//
+// typedef enum {
+//   LDA,
+//   LDX,
+//   LDY,
+// } ins;
+//
+// typedef struct {
+//   ins ins;
+//   mode mode;
+// } INS;
+//
+//
+// /* 256 diff commands */
+// INS inst[0xFF] = {
+//   [0xA9] = {LDA, IMMEDIATE},
+// };
 
 void LDA() {
   u8 val = RAM[cpu.pc+1];
@@ -87,7 +75,7 @@ void tick() {
   printf("command: 0x%02x\n", command);
 
   /* execute */
-  if (command == 0x49) {
+  if (command == 0xA9) {
     LDA(); cpu.pc+=2;
   }
 
@@ -130,5 +118,6 @@ int main(int argc, char *argv[])
   printf("STX ram.0x4401 %hu 0x%02x\n", RAM[0x4401], RAM[0x4401]);
   printf("STY ram.0x4402 %hu 0x%02x\n", RAM[0x4402], RAM[0x4402]);
 
+  printf("\n" );
   return EXIT_SUCCESS;
 }
